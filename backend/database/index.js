feac93f59b849e21f0dbb2590fdfa67ffd681342
unknown <michelleeepm@gmail.com>
2019-04-22 16:49:10 -0500

    restore db env

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect(
-    'mongodb+srv://mhpm:mhpm1055@cluster0-rlsow.mongodb.net/test?retryWrites=true',
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true
+  })
   .then(db => console.log('Database is connected'))
   .catch(err => console.log(err))
